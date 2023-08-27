1d59759f88ee06e9514f25947b0a57608cde4153
jiya10208 <jiya102082004@gmail.com>
2023-08-27 11:31:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ let restartBtn = document.getElementById("restartBtn");
 let winnerIndicator = getComputedStyle(document.body).getPropertyValue(
   "--winning-blocks"
 );
-let boxes = Array.from(document.getElementsByClassName("box"));
-
+let xpoint = 0;
+let opoint = 0;
 let player1 = document.getElementById("p1");
 let player2 = document.getElementById("p2");
-
+let boxes = Array.from(document.getElementsByClassName("box"));
 const o_text = "O";
+
 const x_text = "X";
 let currentPlayer = x_text;
 let spaces = Array(9).fill(null);
@@ -16,6 +17,25 @@ let spaces = Array(9).fill(null);
 const startgame = () => {
   boxes.forEach((box) => box.addEventListener("click", boxclick));
 };
+
+function nulcountcheck() {
+  var nullcount = 0;
+  for (let i = 0; i < spaces.length; i++) {
+    if (!spaces[i]) {
+      nullcount++;
+    }
+  }
+  if (nullcount == 0) {
+    playerText.innerHTML = `tie game`;
+    setTimeout(() => {
+      restart();
+    }, 3000);
+
+    setTimeout(() => {
+      playerText.innerHTML = "TIC TAC TOE";
+    }, 4000);
+  }
+}
 
 function boxclick(e) {
   // console.log(e.target)
@@ -25,38 +45,20 @@ function boxclick(e) {
     spaces[id] = currentPlayer;
     e.target.innerText = currentPlayer;
 
-    if (!spaces[id]) {
-      spaces[id] = currentPlayer;
-      e.target.innerText = currentPlayer;
-  
-      var nullcount = 0;
-      for (let i = 0; i < spaces.length; i++) {
-        if (!spaces[i]) {
-          nullcount++;
-        }
-      }
-      if (nullcount == 0) {
-        playerText.innerHTML = `tie game`;
-        setTimeout(() => {
-          restart();
-        }, 3000);
-  
-        setTimeout(() => {
-          playerText.innerHTML = "TIC TAC TOE";
-        }, 4000);
-      }
+    nulcountcheck();
 
-      
-      
     if (playerhaswon() !== false) {
       playerText.innerHTML = `${currentPlayer} has won`;
+      currentPlayer == x_text ? xpoint++ : opoint++;
+      player1.innerHTML = "X PLAYER " + xpoint;
+      player2.innerHTML = "O PLAYER " + opoint;
       let winning_blocks = playerhaswon();
       setTimeout(() => {
         restart();
       }, 3000);
       setTimeout(() => {
         playerText.innerHTML = "TIC TAC TOE";
-      }, 2000);
+      }, 4000);
 
       winning_blocks.map(
         (box) => (boxes[box].style.backgroundColor = winnerIndicator)
@@ -75,6 +77,7 @@ function boxclick(e) {
       : (player2.style.backgroundColor = "black");
   }
 }
+
 const winningcombo = [
   [0, 1, 2],
   [3, 4, 5],
@@ -103,8 +106,10 @@ function restart() {
     box.innerText = "";
     box.style.backgroundColor = "";
   });
-  playerText = "Tic Tac Toe";
+
+  playerText.innerHTML = `TIC TAC TOE`;
   currentPlayer = x_text;
+  oneplay = false;
 }
 
 restartBtn.addEventListener("click", restart);
